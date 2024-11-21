ecdbc8fda5187b1d46afac1a4d45dc9040284cf7
Noé Viricel <38253764+wazolab@users.noreply.github.com>
2024-11-21 10:52:44 +0100

    Setup testing library (#31)
    * feat: add vitest testing library #29
    
    * feat: add tests to CI #29
    
    * test: TeritorioCluster constructor and defaults #29

---
+++ tests/teritorio-cluster.test.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { TeritorioCluster } from '../src/teritorio-cluster'
+import { Map as MapGL } from 'maplibre-gl'
+
+describe('TeritorioCluster', () => {
+  let map: MapGL
+  let teritorioCluster: TeritorioCluster
+
+  beforeEach(() => {
+    // Create a mock map
+    map = new MapGL({ container: 'map'})
+
+    // Initialize the cluster
+    teritorioCluster = new TeritorioCluster(map, 'sourceId')
+  })
+
+  it('should initialize with default values', () => {
+    expect(teritorioCluster.map).toMatchObject(map)
+    expect(teritorioCluster.clusterLeaves).toBeInstanceOf(Map)
+    expect(teritorioCluster.clusterLeaves.size).toBe(0)
+    expect(teritorioCluster.clusterMaxZoom).toBe(17)
+    expect(teritorioCluster.clusterMinZoom).toBe(0)
+
+    // Should have the default render function
+    expect(teritorioCluster.clusterRender).toBeUndefined()
+
+    expect(teritorioCluster.featuresMap).toBeInstanceOf(Map)
+    expect(teritorioCluster.featuresMap.size).toBe(0)
+    expect(teritorioCluster.fitBoundsOptions).toMatchObject({ padding: 20 })
+    expect(teritorioCluster.initialFeature).toBeUndefined()
+
+    // Should have the default render function
+    expect(teritorioCluster.markerRender).toBeUndefined()
+
+    expect(teritorioCluster.markerSize).toBe(24)
+    expect(teritorioCluster.markersOnScreen).toBeInstanceOf(Map)
+    expect(teritorioCluster.markersOnScreen.size).toBe(0)
+    expect(teritorioCluster.pinMarker).toBeNull()
+
+    // Should have the default render function
+    expect(teritorioCluster.pinMarkerRender).toBeUndefined()
+
+    expect(teritorioCluster.selectedClusterId).toBeNull()
+    expect(teritorioCluster.selectedFeatureId).toBeNull()
+    expect(teritorioCluster.sourceId).toBe('sourceId')
+    expect(teritorioCluster.ticking).toBeFalsy()
+
+    // Should have the default render function
+    expect(teritorioCluster.unfoldedClusterRender).toBeUndefined()
+
+    expect(teritorioCluster.unfoldedClusterMaxLeaves).toBe(7)
+  })
+})
