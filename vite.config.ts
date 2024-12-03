0e5cec994f51c4068dded9f7ae86e807c2bb9d4b
Noé Viricel <noe.viricel@wazo-lab.io>
2024-12-03 14:23:49 +0100

    chore(build): build demo as a separate process  #41

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,44 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  base: '/maplibre-gl-teritorio-cluster/',
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'MaplibreGlTeritorioCluster',
-      fileName: 'maplibre-gl-teritorio-cluster',
-    },
-    sourcemap: true,
-    rollupOptions: {
-      external: ['maplibre-gl'], // Exclude maplibre-gl from the bundle
-      output: {
-        globals: {
-          'maplibre-gl': 'maplibregl', // Define the global variable for maplibre-gl in the browser
+export default defineConfig(({ command, mode }) => {
+  // Demo build config
+  if (command === 'build' && mode === 'demo') {
+    return {
+      base: '/maplibre-gl-teritorio-cluster/',
+      build: {
+        outDir: './demo',
+      },
+    }
+  }
+
+  // Library build config
+  if (command === 'build') {
+    return {
+      build: {
+        lib: {
+          entry: './src/index.ts',
+          name: 'MaplibreGlTeritorioCluster',
+          fileName: 'maplibre-gl-teritorio-cluster',
+        },
+        sourcemap: true,
+        rollupOptions: {
+          external: ['maplibre-gl'], // Exclude maplibre-gl from the bundle
+          output: {
+            globals: {
+              'maplibre-gl': 'maplibregl', // Define the global variable for maplibre-gl in the browser
+            },
+          },
         },
       },
-    },
-  },
-  plugins: [
-    dts({
-      rollupTypes: true,
-    }),
-  ],
-  server: {
-    open: './demo/index.html',
-  },
+      plugins: [
+        dts({
+          rollupTypes: true,
+        }),
+      ],
+    }
+  }
+
+  // Local Development config
+  return {}
 })
