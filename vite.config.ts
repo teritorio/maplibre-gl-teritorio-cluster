bfd24ea13fd43ab5118f54d08e8c1530a3b4201b
Noé Viricel <noe.viricel@wazo-lab.io>
2024-12-03 10:41:54 +0100

    chore(build): set basic build with sourcemaps #41

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,23 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ command, mode }) => {
-  return {
-    base: command === 'build' ? '/maplibre-gl-teritorio-cluster/' : '/',
-    build: mode === 'production'
-      ? {
-          lib: {
-            entry: './src/index.ts',
-            name: 'MaplibreGlTeritorioCluster',
-            fileName: 'maplibre-gl-teritorio-cluster',
-          },
-          rollupOptions: {
-            external: ['maplibre-gl'], // Exclude maplibre-gl from the bundle
-            output: {
-              globals: {
-                'maplibre-gl': 'maplibregl', // Define the global variable for maplibre-gl in the browser
-              },
-            },
-          },
-        }
-      : undefined,
-    server: {
-      open: './demo/index.html',
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'MaplibreGlTeritorioCluster',
+      fileName: 'maplibre-gl-teritorio-cluster',
     },
-  }
+    sourcemap: true,
+    rollupOptions: {
+      external: ['maplibre-gl'], // Exclude maplibre-gl from the bundle
+      output: {
+        globals: {
+          'maplibre-gl': 'maplibregl', // Define the global variable for maplibre-gl in the browser
+        },
+      },
+    },
+  },
+  server: {
+    open: './demo/index.html',
+  },
 })
